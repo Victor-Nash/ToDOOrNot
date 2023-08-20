00cd1ba711caa68c04b3fbeeb533922da361789b
Victor-N <victornashillustration@gmail.com>
2023-08-20 16:05:50 +0200

    first commit

---
+++ JS/APIFetch.js
@@ -0,0 +1,42 @@
+const listContainer = document.querySelector('.list-container');
+const urlTask = 'http://localhost:3000/tasks';
+
+async function createList(task) {
+    await fetch(urlTask, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(task)
+    });
+}
+
+async function getTasks() {
+    let result = await fetch(urlTask);
+    let data = await result.json(); // Cambiado de data.json() a result.json()
+    return data;
+}
+async function deleteTask(id){
+    console.log('Deleting task with ID:', id);
+    await fetch(`${urlTask}/${id}`, {method: 'DELETE'})
+    console.log('Task deleted',urlTask + `/${id}` );
+    
+}
+
+
+async function printTasks() {
+    let tasks = await getTasks();
+    listContainer.innerHTML = '';
+    tasks.forEach(task => {
+        listContainer.innerHTML += 
+        `<div>
+            <h1>${task.title}</h1>
+            <p>${task.priority}</p> 
+            <p>${task.stage}
+            <button onClick="deleteTask(${task.id})" onClick= printTasks()>Eliminar Tarea</button> <!-- Corregido: Eliminar Libro -> Eliminar Tarea -->
+            <button onClick="editTask(${task.id})">Editar Tarea</button> <!-- Corregido: Eliminar Libro -> Editar Tarea -->
+        </div>`;
+    });
+}
+
+
