039e3991c4cf38d41d82e4af3e0c12379a9f6df3
Victor-N <victornashillustration@gmail.com>
2023-11-07 10:28:48 +0100

    feature(JS)updated CRUD

--- JS/formInput.js
+++ JS/formInput.js
@@ -1,5 +1,29 @@
 const formAddTask = document.querySelector('.form-add');
 
+
+async function printTasks(){
+    const listContainer = document.querySelector('.list-elements');
+    await deleteTask();
+    let tasks = await getTasks();
+    listContainer.innerHTML = '';
+    tasks.forEach(task => {
+        listContainer.innerHTML += //Convertir a método.
+        `<li class="list-element"  id="list-element${task.id}">
+            <input class= "task-checkbox" type="checkbox" id="task${task.id}"/>
+            <label for="task${task.id}"class="title-task">${task.title}</label>
+            <span class"custom-checkbox"></span>
+            <p class = "list-priority">Task priority: ${task.priority}</p> 
+            <p class = "list-stage">Stage of completion: ${task.stage}</p>
+            <p class = "list-stage">Coment: ${task.coment}</p>
+            <button class = "btn delete-btn" OnClick="confirmDelete(${task.id})">Eliminar Tarea</button> 
+            <button  class = "btn edit-btn "OnClick="createFormEddit(${task.id})">Editar Tarea</button>
+            <form class = "form-edit${task.id}  form-edit form"></form>
+        </li>`;
+    });
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
         
     formAddTask.addEventListener('submit', (e) => {
